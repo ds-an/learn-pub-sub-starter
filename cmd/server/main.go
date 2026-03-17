89d4b7dff169c177e331b0356d11b32883e840f0
Andrew Malyukov <ds-an@tuta.io>
2026-03-17 17:20:38 +0200

    feat: add a REPL loop taking in some basic commands to the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -13,6 +12,7 @@ import (
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	fmt.Println("Console on http://localhost:15672/")
 	const connectionString = "amqp://guest:guest@localhost:5672/"
 	connection, err := amqp.Dial(connectionString)
 	if err != nil {
@@ -23,16 +23,40 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
-		IsPaused: true,
-	})
-	if err != nil {
-		log.Fatal(err)
+	gamelogic.PrintServerHelp()
+	for {
+		line := gamelogic.GetInput()
+		if len(line) == 0 {
+			continue
+		}
+		switch line[0] {
+		case routing.PauseKey:
+			log.Printf("Sending \"%s\" message", routing.PauseKey)
+			err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+		case "resume":
+			log.Print("Sending \"resume\" message")
+			err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+		case "quit":
+			log.Print("Exiting the game")
+			return
+		default:
+			log.Print("No such command")
+		}
 	}
 
-	fmt.Println("AMQP connection successful")
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("Shutting down Peril server...")
+	// fmt.Println("AMQP connection successful")
+	// signalChan := make(chan os.Signal, 1)
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan
+	// fmt.Println("Shutting down Peril server...")
 }
